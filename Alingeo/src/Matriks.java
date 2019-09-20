1f79cd6e511e2fdfff82d5ceb7cd5b4585ff5645
Aqil <aqilabdaziz1123@gmail.com>
2019-09-20 21:39:50 +0800

    inverse sudah jadi keknya

--- Alingeo/src/Matriks.java
+++ Alingeo/src/Matriks.java
@@ -120,7 +120,7 @@ public class MATRIKS{
     public void printmatriks(){
         for(int i = 1; i <=this.row;i++){
             for(int j = 1; j<=this.col;j++){
-                System.out.print(String.format("%.15f",getElmt(i,j)));
+                System.out.print(String.format("%.15f ",getElmt(i,j)));
             }
             System.out.println("");
         }
@@ -135,7 +135,7 @@ public class MATRIKS{
 
     public boolean eqsize(MATRIKS M){
         return (this.row == M.row && this.col == M.col); 
-    }
+    }   
 
     public boolean eq(MATRIKS M){
         if (!eqsize(M)){ 
@@ -179,11 +179,67 @@ public class MATRIKS{
         return a;
     }
 
-    // //gaussian
-    // public solveSPL(MATRIKS res){
+    //gaussian
+    // public GaussElim(MATRIKS res){
+    //     double divider;
+    //     boolean zeroall;
+
+    //     //PIVOTING
+
         
     // }
+    
+    public MATRIKS inverse(){
+        MATRIKS I = new MATRIKS(this.row,this.col);
+        for (int i = 1; i <= this.row;i ++){
+            for (int j = 1; j<= this.col;j++){
+                I.matrix[i][j] = 0;
+                if (i == j){            //MATRIKS IDENTITAS
+                    I.matrix[i][j] = 1;
+                }
+            }
+        }
 
+        if (this.bujur){
+            for (int l =1; l <=this.row;l++){    
+                double divider = (1 / this.matrix[l][l]);
+                for (int k = l; k <= this.col;k++){             //dividing
+                    this.matrix[l][k] *= divider;
+                    I.matrix[l][k] *= divider;    
+                }
+                for (int m = 1; m <= this.row; m++){
+                    if ((m != l) && (this.matrix[l][m] != 0)){
+                        double substractor = this.matrix[l][m];
+                        for (int n = 1; n <= this.col;n++){
+                            this.matrix[m][n] -= substractor * this.matrix[l][n]; 
+                            I.matrix[m][n] -= substractor * I.matrix[l][n];
+                        }
+                    }
+                }
+            }
+            return I;
+        }
+        else{
+            return this;
+            System.out.println("ilegal dimension, no inverse");
+        }
+    }
+
+    // MEMBUAT MATRIKS JADI SEGITIGA ATAS
+    // public MATRIKS uppertri(){
+    //     int col = this.col;
+    //     int row = this.row;
+    // for (int i = 1; i<= col;i++){
+    //     int max = i;
+    //         for (int j = i + 1; j <= a.col; j++){
+    //             if (Math.abs(a.Elmt(j,i)) > Math.abs(a.Elmt(max,i))){
+    //                 max = j;
+    //             }
+    //         a.swap(i, max); 
+    //         b.swap(i, max);  
+    //         }
+    // }
+    // }
     // //gaussjordan
     // public solve2(){
 
